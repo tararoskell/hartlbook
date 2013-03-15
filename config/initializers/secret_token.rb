9370876c8c65d32a1f1a63d899bd01c60e1ea5c5
tararoskell <tara@troskell.demon.co.uk>
2013-03-15 14:57:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartlbook::Application.config.secret_token = 'd31b880aa3c7a3b2197f16b5f0185b4c94245aee28adaf0ce6d85c656200fdb066949ccb0326137496cbf2c618d42a58317856b33cd380ae95a3076e0863570b'
